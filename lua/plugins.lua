be854155580c524791bba45e3671a95548ee9910
a.khakimov <a.khakimov@tinkoff.ru>
2022-10-09 02:08:17 +0500

    Tree and bufferline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,14 +15,16 @@ return require('packer').startup(function()
         end,
     }
     
-    use { 'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} },
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
         config = function()
             require('telescope').setup {}
         end,
     }
 
-    use { 'nvim-telescope/telescope-fzf-native.nvim',
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
         run = 'make'
     }
 
@@ -31,5 +33,38 @@ return require('packer').startup(function()
         config = function()
             require('gitsigns').setup {}
         end,
+    }
+
+    use {
+        'kyazdani42/nvim-web-devicons'
+    }
+
+    use {
+        "akinsho/toggleterm.nvim", tag = '*',
+        config = function()
+            require("toggleterm").setup()
+        end,
+    }
+
+    
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "v2.*",
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            vim.opt.termguicolors = true
+            require("bufferline").setup()
+        end,
+    }
+
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+        config = function()
+            require("nvim-tree").setup()
+        end,
+        tag = 'nightly' -- optional, updated every week. (see issue #1193)
     }
 end)
